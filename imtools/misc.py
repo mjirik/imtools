19ceed9e318f824402aab95f42aa13014b582d6e
M. Jirik <miroslav.jirik@gmail.com>
2015-10-18 12:22:32 +0200

    new modules

---
+++ imtools/misc.py
@@ -0,0 +1,211 @@
+#! /usr/bin/python
+# -*- coding: utf-8 -*-
+
+# import sys
+import os
+
+import logging
+logger = logging.getLogger(__name__)
+
+import sys
+import os.path
+
+path_to_script = os.path.dirname(os.path.abspath(__file__))
+sys.path.append(os.path.join(path_to_script, "./extern/sPickle"))
+# import numpy as np
+
+
+def suggest_filename(file_path, exists=None):
+    """
+    Try if exist path and append number to its end.
+    For debug you can set as input if file exists or not.
+    """
+    import os.path
+    import re
+    if not isinstance(exists, bool):
+        exists = os.path.exists(file_path)
+
+    if exists:
+        file_path, file_extension = os.path.splitext(file_path)
+        # print file_path
+        m = re.search(r"\d+$", file_path)
+        if m is None:
+            # cislo = 2
+            new_cislo_str = "2"
+        else:
+            cislostr = (m.group())
+            cislo = int(cislostr) + 1
+            file_path = file_path[:-len(cislostr)]
+            new_cislo_str = str(cislo)
+
+        file_path = file_path + new_cislo_str + file_extension  # .zfill(2)
+        # trorcha rekurze
+        file_path = suggest_filename(file_path)
+
+    return file_path
+
+
+def obj_from_file(filename='annotation.yaml', filetype='yaml'):
+    ''' Read object from file '''
+
+    if filetype == 'auto':
+        _, ext = os.path.splitext(filename)
+        filetype = ext[1:]
+
+    if filetype == 'yaml':
+        import yaml
+        f = open(filename, 'rb')
+        obj = yaml.load(f)
+        f.close()
+    elif filetype in ('pickle', 'pkl', 'pklz', 'picklezip'):
+        fcontent = read_pkl_and_pklz(filename)
+        # import pickle
+        import cPickle as pickle
+        # import sPickle as pickle
+        obj = pickle.loads(fcontent)
+    else:
+        logger.error('Unknown filetype ' + filetype)
+    return obj
+
+
+def read_pkl_and_pklz(filename):
+    """
+    Try read zipped or not zipped pickle file
+    """
+    fcontent = None
+    try:
+        import gzip
+        f = gzip.open(filename, 'rb')
+        fcontent = f.read()
+        f.close()
+    except IOError as e:
+        # if the problem is in not gzip file
+        logger.warning("Input gzip exception: " + str(e))
+        f = open(filename, 'rb')
+        fcontent = f.read()
+        f.close()
+    except Exception as e:
+        # other problem
+        import traceback
+        logger.error("Input gzip exception: " + str(e))
+        logger.error(traceback.format_exc())
+
+    return fcontent
+
+
+def obj_to_file(obj, filename='annotation.yaml', filetype='yaml'):
+    '''Writes annotation in file.
+
+    Filetypes:
+        yaml
+        pkl, pickle
+        pklz, picklezip
+    '''
+    # import json
+    # with open(filename, mode='w') as f:
+    #    json.dump(annotation,f)
+
+    # write to yaml
+    d = os.path.dirname(os.path.abspath(filename))
+    if not os.path.exists(d):
+        os.makedirs(d)
+
+    if filetype == 'auto':
+        _, ext = os.path.splitext(filename)
+        filetype = ext[1:]
+
+    if filetype == 'yaml':
+        f = open(filename, 'wb')
+        import yaml
+        yaml.dump(obj, f)
+        f.close
+    elif filetype in ('pickle', 'pkl'):
+        f = open(filename, 'wb')
+        import cPickle as pickle
+        pickle.dump(obj, f)
+        f.close
+    elif filetype in ('streamingpicklezip', 'spklz'):
+        # this is not working :-(
+        import gzip
+        import sPickle as pickle
+        f = gzip.open(filename, 'wb', compresslevel=1)
+        # f = open(filename, 'wb')
+        pickle.s_dump(obj, f)
+        f.close
+    elif filetype in ('picklezip', 'pklz'):
+        import gzip
+        import cPickle as pickle
+        f = gzip.open(filename, 'wb', compresslevel=1)
+        # f = open(filename, 'wb')
+        pickle.dump(obj, f)
+        f.close
+    elif filetype in('mat'):
+
+        import scipy.io as sio
+        sio.savemat(filename, obj)
+    else:
+        logger.error('Unknown filetype ' + filetype)
+
+
+def resize_to_shape(data, shape, zoom=None, mode='nearest', order=0):
+    """
+    Function resize input data to specific shape.
+
+    :param data: input 3d array-like data
+    :param shape: shape of output data
+    :param zoom: zoom is used for back compatibility
+    :mode: default is 'nearest'
+    """
+    # @TODO remove old code in except part
+
+    try:
+        # rint 'pred vyjimkou'
+        # aise Exception ('test without skimage')
+        # rint 'za vyjimkou'
+        import skimage
+        import skimage.transform
+# Now we need reshape  seeds and segmentation to original size
+
+        segm_orig_scale = skimage.transform.resize(
+            data, shape, order=0,
+            preserve_range=True
+        )
+
+        segmentation = segm_orig_scale
+        logger.debug('resize to orig with skimage')
+    except:
+        import scipy
+        import scipy.ndimage
+        dtype = data.dtype
+        if zoom is None:
+            zoom = shape / np.asarray(data.shape).astype(np.double)
+
+        segm_orig_scale = scipy.ndimage.zoom(
+            data,
+            1.0 / zoom,
+            mode=mode,
+            order=order
+        ).astype(dtype)
+        logger.debug('resize to orig with scipy.ndimage')
+
+# @TODO odstranit hack pro oříznutí na stejnou velikost
+# v podstatě je to vyřešeno, ale nechalo by se to dělat elegantněji v zoom
+# tam je bohužel patrně bug
+        # rint 'd3d ', self.data3d.shape
+        # rint 's orig scale shape ', segm_orig_scale.shape
+        shp = [
+            np.min([segm_orig_scale.shape[0], shape[0]]),
+            np.min([segm_orig_scale.shape[1], shape[1]]),
+            np.min([segm_orig_scale.shape[2], shape[2]]),
+        ]
+        # elf.data3d = self.data3d[0:shp[0], 0:shp[1], 0:shp[2]]
+        # mport ipdb; ipdb.set_trace() # BREAKPOINT
+
+        segmentation = np.zeros(shape, dtype=dtype)
+        segmentation[
+            0:shp[0],
+            0:shp[1],
+            0:shp[2]] = segm_orig_scale[0:shp[0], 0:shp[1], 0:shp[2]]
+
+        del segm_orig_scale
+    return segmentation
---
+++ imtools/qmisc.py
@@ -0,0 +1,281 @@
+#! /usr/bin/python
+# -*- coding: utf-8 -*-
+
+
+import sys
+import os.path
+
+path_to_script = os.path.dirname(os.path.abspath(__file__))
+sys.path.append(os.path.join(path_to_script, "../extern/sed3"))
+
+import numpy as np
+
+import logging
+logger = logging.getLogger(__name__)
+
+import subprocess
+import scipy
+import scipy.ndimage
+
+
+class SparseMatrix():
+    def __init__(self, ndarray):
+        self.coordinates = ndarray.nonzero()
+        self.shape = ndarray.shape
+        self.values = ndarray[self.coordinates]
+        self.dtype = ndarray.dtype
+        self.sparse = True
+
+    def todense(self):
+        dense = np.zeros(self.shape, dtype=self.dtype)
+        dense[self.coordinates[:]] = self.values
+        return dense
+
+
+def isSparseMatrix(obj):
+    if obj.__class__.__name__ == 'SparseMatrix':
+        return True
+    else:
+        return False
+
+
+# import sed3
+
+def manualcrop(data):  # pragma: no cover
+
+    try:
+        from pysegbase import seed_editor_qt
+    except:
+        logger.warning("Deprecated of pyseg_base as submodule")
+        import seed_editor_qt
+
+    pyed = seed_editor_qt.QTSeedEditor(data, mode='crop')
+    pyed.exec_()
+    # pyed = sed3.sed3(data)
+    # pyed.show()
+    nzs = pyed.seeds.nonzero()
+    crinfo = [
+        [np.min(nzs[0]), np.max(nzs[0])],
+        [np.min(nzs[1]), np.max(nzs[1])],
+        [np.min(nzs[2]), np.max(nzs[2])],
+        ]
+    data = crop(data, crinfo)
+    return data, crinfo
+
+
+def crop(data, crinfo):
+    """
+    Crop the data.
+
+    crop(data, crinfo)
+
+    :param crinfo: min and max for each axis
+
+    """
+    crinfo = fix_crinfo(crinfo)
+    return data[
+        __int_or_none(crinfo[0][0]):__int_or_none(crinfo[0][1]),
+        __int_or_none(crinfo[1][0]):__int_or_none(crinfo[1][1]),
+        __int_or_none(crinfo[2][0]):__int_or_none(crinfo[2][1])
+        ]
+
+
+def __int_or_none(number):
+    if number is not None:
+        number = int(number)
+    return number
+
+
+def combinecrinfo(crinfo1, crinfo2):
+    """
+    Combine two crinfos. First used is crinfo1, second used is crinfo2.
+    """
+    crinfo1 = fix_crinfo(crinfo1)
+    crinfo2 = fix_crinfo(crinfo2)
+
+    crinfo = [
+        [crinfo1[0][0] + crinfo2[0][0], crinfo1[0][0] + crinfo2[0][1]],
+        [crinfo1[1][0] + crinfo2[1][0], crinfo1[1][0] + crinfo2[1][1]],
+        [crinfo1[2][0] + crinfo2[2][0], crinfo1[2][0] + crinfo2[2][1]]
+        ]
+
+    return crinfo
+
+
+def crinfo_from_specific_data(data, margin):
+    # hledáme automatický ořez, nonzero dá indexy
+    logger.debug('crinfo')
+    logger.debug(str(margin))
+    nzi = np.nonzero(data)
+    logger.debug(str(nzi))
+
+    x1 = np.min(nzi[0]) - margin[0]
+    x2 = np.max(nzi[0]) + margin[0] + 1
+    y1 = np.min(nzi[1]) - margin[0]
+    y2 = np.max(nzi[1]) + margin[0] + 1
+    z1 = np.min(nzi[2]) - margin[0]
+    z2 = np.max(nzi[2]) + margin[0] + 1
+
+# ošetření mezí polí
+    if x1 < 0:
+        x1 = 0
+    if y1 < 0:
+        y1 = 0
+    if z1 < 0:
+        z1 = 0
+
+    if x2 > data.shape[0]:
+        x2 = data.shape[0] - 1
+    if y2 > data.shape[1]:
+        y2 = data.shape[1] - 1
+    if z2 > data.shape[2]:
+        z2 = data.shape[2] - 1
+
+# ořez
+    crinfo = [[x1, x2], [y1, y2], [z1, z2]]
+    return crinfo
+
+
+def uncrop(data, crinfo, orig_shape, resize=False):
+
+    crinfo = fix_crinfo(crinfo)
+    data_out = np.zeros(orig_shape, dtype=data.dtype)
+
+    # print 'uncrop ', crinfo
+    # print orig_shape
+    # print data.shape
+    if resize:
+        data = resize_to_shape(data, crinfo[:, 1] - crinfo[:, 0])
+
+    startx = np.round(crinfo[0][0]).astype(int)
+    starty = np.round(crinfo[1][0]).astype(int)
+    startz = np.round(crinfo[2][0]).astype(int)
+
+    data_out[
+        # np.round(crinfo[0][0]).astype(int):np.round(crinfo[0][1]).astype(int)+1,
+        # np.round(crinfo[1][0]).astype(int):np.round(crinfo[1][1]).astype(int)+1,
+        # np.round(crinfo[2][0]).astype(int):np.round(crinfo[2][1]).astype(int)+1
+        startx:startx + data.shape[0],
+        starty:starty + data.shape[1],
+        startz:startz + data.shape[2]
+        ] = data
+
+    return data_out
+
+
+def fix_crinfo(crinfo, to='axis'):
+    """
+    Function recognize order of crinfo and convert it to proper format.
+    """
+
+    crinfo = np.asarray(crinfo)
+    if crinfo.shape[0] == 2:
+        crinfo = crinfo.T
+
+    return crinfo
+
+
+def getVersionString():
+    """
+    Function return string with version information.
+    It is performed by use one of three procedures: git describe,
+    file in .git dir and file __VERSION__.
+    """
+    version_string = None
+    try:
+        version_string = subprocess.check_output(['git', 'describe'])
+    except:
+        logger.warning('Command "git describe" is not working')
+
+    if version_string == None:  # noqa
+        try:
+            path_to_version = os.path.join(path_to_script,
+                                           '../.git/refs/heads/master')
+            with file(path_to_version) as f:
+                version_string = f.read()
+        except:
+            logger.warning('Problem with reading file ".git/refs/heads/master"')
+
+    if version_string == None:  # noqa
+        try:
+            path_to_version = os.path.join(path_to_script, '../__VERSION__')
+            with file(path_to_version) as f:
+                version_string = f.read()
+            path_to_version = path_to_version + \
+                '  version number is created manually'
+
+        except:
+            logger.warning('Problem with reading file "__VERSION__"')
+
+    return version_string
+
+
+def get_one_biggest_object(data):
+    """ Return biggest object """
+    lab, num = scipy.ndimage.label(data)
+    # print ("bum = "+str(num))
+
+    maxlab = max_area_index(lab, num)
+
+    data = (lab == maxlab)
+    return data
+
+
+def max_area_index(labels, num):
+    """
+    Return index of maxmum labeled area
+    """
+    mx = 0
+    mxi = -1
+    for l in range(1, num + 1):
+        mxtmp = np.sum(labels == l)
+        if mxtmp > mx:
+            mx = mxtmp
+            mxi = l
+
+    return mxi
+
+
+def resize_to_mm(data3d, voxelsize_mm, new_voxelsize_mm, mode='nearest'):
+    """
+    Function can resize data3d or segmentation to specifed voxelsize_mm
+    :new_voxelsize_mm: requested voxelsize. List of 3 numbers, also 
+        can be a string 'orig', 'orgi*2' and 'orgi*4'.
+        
+    :voxelsize_mm: size of voxel
+    :mode: default is 'nearest'
+    """
+
+    if new_voxelsize_mm == 'orig':
+        new_voxelsize_mm = np.array(voxelsize_mm)
+
+    elif new_voxelsize_mm == 'orig*2':
+        new_voxelsize_mm = np.array(voxelsize_mm) * 2
+    elif new_voxelsize_mm == 'orig*4':
+        new_voxelsize_mm = np.array(voxelsize_mm) * 4
+        # vx_size = np.array(metadata['voxelsize_mm']) * 4
+
+    zoom = voxelsize_mm / (1.0 * np.array(new_voxelsize_mm))
+    data3d_res = scipy.ndimage.zoom(
+        data3d,
+        zoom,
+        mode=mode,
+        order=1
+    ).astype(data3d.dtype)
+    return data3d_res
+
+
+def resize_to_shape(*pars, **params):
+    import misc
+    return misc.resize_to_shape(*pars, **params)
+
+    # nzoom = shape / np.array(segmentation.shape).astype(np.double)
+    #
+    # segm_orig_scale = scipy.ndimage.zoom(
+    #     segmentation,
+    #     nzoom,
+    #     mode='nearest',
+    #     order=0
+    # ).astype(segmentation.dtype)
+    #
+    # return segm_orig_scale
