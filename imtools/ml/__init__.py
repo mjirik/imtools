8c1d80003de27414c30441bdc8b2e4de447882a1
Miroslav Jirik <miroslav.jirik@gmail.com>
2015-11-17 11:14:26 +0100

    new gm classifier

---
+++ imtools/ml/__init__.py
@@ -0,0 +1,8 @@
+__all__ = ['gmmcl' ]
+
+import gmmcl
+# import misc
+# from qmisc import resize_to_mm, resize_to_shape
+# from import Model, ImageGraphCut
+# from seed_editor_qt import QTSeedEditor
+# from dcmreaddata import DicomReader
---
+++ imtools/ml/gmmcl.py
@@ -0,0 +1,73 @@
+#! /usr/bin/python
+# -*- coding: utf-8 -*-
+
+# import sys
+import os
+import sys
+import os.path
+import numpy as np
+
+import logging
+logger = logging.getLogger(__name__)
+import sklearn
+
+# path_to_script = os.path.dirname(os.path.abspath(__file__))
+# sys.path.append(os.path.join(path_to_script, "./extern/sPickle"))
+
+class GMMCl():
+    def __init__(self, **pars):
+        self.clspars = pars
+        self.cls = {}
+
+    def fit(self, data, target):
+        data = np.asarray(data)
+        target = np.asarray(target).astype(np.int16)
+#         print "gmmcl fit sh ", data.shape
+        un = np.unique(target)
+        for label in un:
+            cli = sklearn.mixture.GMM(**self.clspars)
+#             print "fr ", data.shape, target.shape
+            dtl = data[target.reshape(-1)==label]
+#             print "data target label ", data.shape, dtl.shape
+            cli.fit(dtl)
+            self.cls[label] = cli
+            #if label in self.cls.keys():
+            #    pass
+            #else:
+
+        pass
+
+    def __relabel(self, target, new_keys):
+        out = np.zeros(target.shape, dtype=target.dtype)
+        for label, i in new_keys.iteritems():
+            out[target==i] = label
+
+        return out
+
+    def scores(self, x):
+        x = np.asarray(x)
+#         print 'gmmcl predict shape ', x.shape
+        score = {}
+        #score = []
+
+        for label in self.cls.keys():
+#             print "for ", i, label, x.shape, self.cls.keys()
+
+            sc = self.cls[label].score(x)
+            score[label] = sc
+        return score
+
+    def predict(self, x):
+        x = np.asarray(x)
+#         print 'gmmcl predict shape ', x.shape
+        score_l = {}
+        score = []
+
+        for i, label in enumerate(self.cls.keys()):
+#             print "for ", i, label, x.shape, self.cls.keys()
+
+            score_l[label] = i
+            sc = self.cls[label].score(x)
+            score.append(sc)
+        target_tmp = np.argmax(score, 0)
+        return self.__relabel(target_tmp, score_l)
