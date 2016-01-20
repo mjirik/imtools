50d3e65d2ae90239918b3c999c0e2af0310b635e
Miroslav Jirik <miroslav.jirik@gmail.com>
2016-01-21 00:03:10 +0100

    imtools instead of lisa

--- docs/conf.py
+++ docs/conf.py
@@ -228,7 +228,7 @@ html_static_path = ['_static']
 #html_file_suffix = None
 
 # Output file base name for HTML help builder.
-htmlhelp_basename = 'lisadoc'
+htmlhelp_basename = 'imtoolsdoc'
 
 
 # -- Options for LaTeX output ---------------------------------------------
@@ -248,7 +248,7 @@ latex_elements = {
 # (source start file, target name, title,
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
-  ('index', 'lisa.tex', u'lisa Documentation',
+  ('index', 'imtools.tex', u'lisa Documentation',
    u'M. Jirik', 'manual'),
 ]
 
@@ -278,7 +278,7 @@ latex_documents = [
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
 man_pages = [
-    ('index', 'lisa', u'lisa Documentation',
+    ('index', 'imtools', u'lisa Documentation',
      [u'M. Jirik'], 1)
 ]
 
@@ -292,8 +292,8 @@ man_pages = [
 # (source start file, target name, title, author,
 #  dir menu entry, description, category)
 texinfo_documents = [
-  ('index', 'lisa', u'lisa Documentation',
-   u'M. Jirik', 'lisa', 'One line description of project.',
+  ('index', 'imtools', u'lisa Documentation',
+   u'M. Jirik', 'imtools', 'One line description of project.',
    'Miscellaneous'),
 ]
 
