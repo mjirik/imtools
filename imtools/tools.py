b53ac8c17fc6b136195ba068a2cf73f55a1c3435
Tomas Ryba <rybato@gmail.com>
2016-01-28 15:55:58 +0100

    Pridana funkce pro pyramidy - nevyhlazuje a proto je dobra napr. pro HOGy.

--- imtools/tools.py
+++ imtools/tools.py
@@ -1056,3 +1056,61 @@ def arange_figs(imgs, tits=None, max_r=3, max_c=5, colorbar=False, same_range=Fa
         arange_figs(imgs_rem, tits=tits_rem)
     elif show_now:
         plt.show()
+
+
+def resize(image, width=None, height=None, inter=cv2.INTER_AREA):
+    # initialize the dimensions of the image to be resized and
+    # grab the image size
+    dim = None
+    (h, w) = image.shape[:2]
+
+    # if both the width and height are None, then return the
+    # original image
+    if width is None and height is None:
+        return image
+
+    # check to see if the width is None
+    if width is None:
+        # calculate the ratio of the height and construct the
+        # dimensions
+        r = height / float(h)
+        dim = (int(w * r), height)
+
+    # otherwise, the height is None
+    else:
+        # calculate the ratio of the width and construct the
+        # dimensions
+        r = width / float(w)
+        dim = (width, int(h * r))
+
+    # resize the image
+    resized = cv2.resize(image, dim, interpolation=inter)
+
+    # return the resized image
+    return resized
+
+
+def pyramid(image, scale=2, minSize=(30, 30)):
+    '''
+    Creates generator of image pyramid.
+    :param image: input image
+    :param scale: factor that controls by how much the image is resized at each layer
+    :param minSize: minimum required width and height of the layer
+    :return: generator of the image pyramid
+    '''
+    # yield the original image
+    yield image
+
+    # keep looping over the pyramid
+    while True:
+        # compute the new dimensions of the image and resize it
+        w = int(image.shape[1] / scale)
+        image = resize(image, width=w)
+
+        # if the resized image does not meet the supplied minimum
+        # size, then stop constructing the pyramid
+        if image.shape[0] < minSize[1] or image.shape[1] < minSize[0]:
+            break
+
+        # yield the next image in the pyramid
+        yield image
