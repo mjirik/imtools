ead50f09460c3c4442f1a69026cef6378e230e88
Miroslav Jirik <miroslav.jirik@gmail.com>
2016-05-24 22:14:27 +0200

    remove import tools

--- imtools/__init__.py
+++ imtools/__init__.py
@@ -1,9 +1,9 @@
-__all__ = ['qmisc', 'misc', 'tools' ]
+__all__ = ['qmisc', 'misc' ]
 
 import qmisc
 import misc
 from qmisc import resize_to_mm, resize_to_shape
-import tools
+# import tools
 # from import Model, ImageGraphCut
 # from seed_editor_qt import QTSeedEditor
 # from dcmreaddata import DicomReader
