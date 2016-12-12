cf838df879601d7168f8937ef1287359db3aedc9
Miroslav Jirik <miroslav.jirik@gmail.com>
2016-12-12 15:45:12 +0100

    new tests for ui threshold

--- tests/ui_threshold_test.py
+++ tests/ui_threshold_test.py
@@ -3,11 +3,14 @@
 
 import unittest
 from nose.plugins.attrib import attr
+import imtools
 import imtools.sample_data
 import imtools.uiThreshold
+import imtools.thresholding_functions
 import matplotlib.pyplot as plt
 import sys
 from PyQt4.QtGui import QApplication, QDialog, QGridLayout, QPushButton
+import numpy as np
 
 class MyTestCase(unittest.TestCase):
     @attr('interactive')
@@ -31,7 +34,36 @@ class MyTestCase(unittest.TestCase):
         app = QApplication(sys.argv)
         datap = imtools.sample_data.generate()
         uit = imtools.uiThreshold.uiThresholdQt(datap['data3d'], datap['voxelsize_mm'], interactivity=True, threshold=100)
+
         uit.run()
         # plt.show()
+
+    def test_getPriorityObject(self):
+        import skimage.morphology
+        nobj = 2
+        datap = imtools.sample_data.generate()
+        thresholded = datap["data3d"] > 80
+        selection = imtools.thresholding_functions.getPriorityObjects(thresholded, nObj=nobj, seeds_multi_index=None)
+
+        lab = skimage.morphology.label(selection)
+        output_nobj = np.unique(lab)
+
+        self.assertEqual(nobj, nobj)
+
+    def test_getPriorityObjectSeeds(self):
+        import skimage.morphology
+
+        nobj = 1
+        datap = imtools.sample_data.generate()
+        thresholded = datap["data3d"] > 80
+        seeds_multi_index = np.nonzero(datap['seeds'] == 1)
+
+        selection = imtools.thresholding_functions.getPriorityObjects(thresholded, nObj=nobj, seeds_multi_index=seeds_multi_index)
+
+        lab = skimage.morphology.label(selection)
+        output_nobj = np.unique(lab)
+
+        self.assertEqual(nobj, nobj)
+
 if __name__ == '__main__':
     unittest.main()
