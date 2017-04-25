c44fcf55bd2e9cdec0befc4ad6800165bb8ed88f
Miroslav Jirik <miroslav.jirik@gmail.com>
2017-04-25 14:40:05 +0200

    use GaussianMixture instead of GMM

--- imtools/ml/gmmcl.py
+++ imtools/ml/gmmcl.py
@@ -26,15 +26,11 @@ class GMMCl():
 #         print "gmmcl fit sh ", data.shape
         un = np.unique(target)
         for label in un:
-            cli = sklearn.mixture.GMM(**self.clspars)
-#             print "fr ", data.shape, target.shape
+            # cli = sklearn.mixture.GMM(**self.clspars)
+            cli = sklearn.mixture.GaussianMixture(**self.clspars)
             dtl = data[target.reshape(-1)==label]
-#             print "data target label ", data.shape, dtl.shape
             cli.fit(dtl)
             self.cls[label] = cli
-            #if label in self.cls.keys():
-            #    pass
-            #else:
 
         pass
 
@@ -47,26 +43,19 @@ class GMMCl():
 
     def scores(self, x):
         x = np.asarray(x)
-#         print 'gmmcl predict shape ', x.shape
         score = {}
-        #score = []
 
         for label in self.cls.keys():
-#             print "for ", i, label, x.shape, self.cls.keys()
-
             sc = self.cls[label].score(x)
             score[label] = sc
         return score
 
     def predict(self, x):
         x = np.asarray(x)
-#         print 'gmmcl predict shape ', x.shape
         score_l = {}
         score = []
 
         for i, label in enumerate(self.cls.keys()):
-#             print "for ", i, label, x.shape, self.cls.keys()
-
             score_l[label] = i
             sc = self.cls[label].score(x)
             score.append(sc)
