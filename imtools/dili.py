2d5985346b406a45293ad4ec341cf3edab2236de
M. Jirik <miroslav.jirik@gmail.com>
2017-07-26 12:45:19 +0200

    list contains and drop duplicates added

--- imtools/dili.py
+++ imtools/dili.py
@@ -8,6 +8,7 @@ import logging
 logger = logging.getLogger(__name__)
 import collections
 import inspect
+import copy
 
 
 def get_default_args(obj):
@@ -49,7 +50,8 @@ def kick_from_dict(dct, keys):
 
 def split_dict(dct, keys):
     """
-    Split dict into two subdicts based on keys
+    Split dict into two subdicts based on keys.
+
     :param dct:
     :param keys:
     :return: dict_in, dict_out
@@ -75,8 +77,8 @@ def recursive_update(d, u):
     Based on Alex Martelli code on stackoverflow
     http://stackoverflow.com/questions/3232943/update-value-of-a-nested-dictionary-of-varying-depth?answertab=votes#tab-top
 
-    :param d:
-    :param u:
+    :param d: dict to update
+    :param u: dict with new data
     :return:
     """
     for k, v in u.iteritems():
@@ -93,13 +95,23 @@ from operator import add
 
 _FLAG_FIRST = object()
 
-def flatten_dict(d, separator=None, join=add, lift=lambda x:x):
+def flatten_dict_join_keys(dct, join_symbol=" "):
+    """ Flatten dict with defined key join symbol.
+
+    :param dct: dict to flatten
+    :param join_symbol: default value is " "
+    :return:
+    """
+    return dict( flatten_dict(dct, join=lambda a,b:a+join_symbol+b) )
+
+
+def flatten_dict(dct, separator=None, join=add, lift=lambda x:x):
     """
 
     Based on ninjagecko code on stackoveflow
     http://stackoverflow.com/questions/6027558/flatten-nested-python-dictionaries-compressing-keys
 
-    :param d: dict to flatten
+    :param dct: dict to flatten
     :param separator: use preset values for join and lift.
     Use empty list or tuple [], () for key hierarchy stored in list.
     If simple_mode is string it is used as a separator.
@@ -119,8 +131,6 @@ def flatten_dict(d, separator=None, join=add, lift=lambda x:x):
     elif type(separator) in (list, tuple):
         lift = lambda x:(x,)
 
-
-
     results = []
     def visit(subdict, results, partialKey):
         for k,v in subdict.items():
@@ -129,5 +139,37 @@ def flatten_dict(d, separator=None, join=add, lift=lambda x:x):
                 visit(v, results, newKey)
             else:
                 results.append((newKey,v))
-    visit(d, results, _FLAG_FIRST)
+    visit(dct, results, _FLAG_FIRST)
     return results
+
+
+def list_contains(list_of_strings, substring, return_true_false_array=False):
+    """ Get strings in list which contains substring.
+
+    """
+    key_tf = [keyi.find(substring) != -1 for keyi in list_of_strings]
+    if return_true_false_array:
+        return key_tf
+    keys_to_remove = list_of_strings[key_tf]
+    return keys_to_remove
+
+
+def df_drop_duplicates(df, ignore_key_pattern="time"):
+    """
+    Drop duplicates from dataframe ignore columns with keys containing defined pattern.
+
+    :param df:
+    :param noinfo_key_pattern:
+    :return:
+    """
+
+    keys_to_remove = list_contains(df.keys(), ignore_key_pattern)
+    #key_tf = [key.find(noinfo_key_pattern) != -1 for key in df.keys()]
+    # keys_to_remove
+    # remove duplicates
+    ks = copy.copy(list(df.keys()))
+    for key in keys_to_remove:
+        ks.remove(key)
+
+    df = df.drop_duplicates(ks)
+    return df
