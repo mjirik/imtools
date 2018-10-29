8301966d8e58d3da2d7f1f6f6e226478bc45aac1
Miroslav Jirik <miroslav.jirik@gmail.com>
2018-10-29 15:35:25 +0100

    Bump version: 1.0.2 → 1.1.0

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # http://packaging.python.org/en/latest/tutorial.html#version
-    version='1.0.2',
+    version='1.1.0',
     url='https://github.com/mjirik/imtools',
     author='Miroslav Jirik and Tomas Ryba',
     author_email='miroslav.jirik@gmail.com',
